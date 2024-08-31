17f558a948485079ab5cce8daa648ea059f97c79
ZCMR <95862895+ZCMR@users.noreply.github.com>
2024-08-31 17:53:45 +0800

    Add files via upload

---
+++ build_vocab.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# build vocab for different datasets
+
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Restaurants_corenlp --vocab_dir DualGCN/dataset/Restaurants_corenlp
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Laptops_corenlp --vocab_dir DualGCN/dataset/Laptops_corenlp
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Tweets_corenlp --vocab_dir DualGCN/dataset/Tweets_corenlp
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Restaurants_corenlp16 --vocab_dir DualGCN/dataset/Restaurants_corenlp16
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/NAMS_corenlp --vocab_dir DualGCN/dataset/NAMS_corenlp
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Restaurants_corenlp15 --vocab_dir DualGCN/dataset/Restaurants_corenlp15
+
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Restaurants_allennlp --vocab_dir DualGCN/dataset/Restaurants_allennlp
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Laptops_allennlp --vocab_dir DualGCN/dataset/Laptops_allennlp
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Tweets_allennlp --vocab_dir DualGCN/dataset/Tweets_allennlp
+
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Restaurants_stanza --vocab_dir DualGCN/dataset/Restaurants_stanza
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Laptops_stanza --vocab_dir DualGCN/dataset/Laptops_stanza
+python ./DualGCN/prepare_vocab.py --data_dir DualGCN/dataset/Tweets_stanza --vocab_dir DualGCN/dataset/Tweets_stanza
